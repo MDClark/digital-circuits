eeb36c6938879730409a16e89dceafb26923e6eb
MDClark <m14.clark@connect.qut.edu.au>
2014-08-05 20:54:20 +1000

    Part A Partial Implementation
    75% JUnit tests passing, current error message makes no sense.
    
    Not sure how the project will handle the JUnit imports and all the
    .properties/.class/.jar files, let me know if there are issues.

--- DigitalCircuits/src/Digital/OutputTerminal.java
+++ DigitalCircuits/src/Digital/OutputTerminal.java
@@ -7,6 +7,7 @@
 package Digital;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 /**
  *
@@ -14,19 +15,54 @@ import java.beans.PropertyChangeListener;
  */
 public class OutputTerminal implements Terminal {
 
+    private boolean value;
+    public static final String PROP_VALUE = "value";
+
+    /**
+     * Get the value of value
+     *
+     * @return the value of value
+     */
+    public boolean isValue() {
+        return value;
+    }
+
+    /**
+     * Set the value of value
+     *
+     * @param value new value of value
+     */
+    public void setValue(boolean value) {
+        boolean oldValue = this.value;
+        this.value = value;
+        propertyChangeSupport.firePropertyChange(PROP_VALUE, oldValue, value);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    /**
+     * Add PropertyChangeListener.
+     *
+     * @param listener
+     */
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Remove PropertyChangeListener.
+     *
+     * @param listener
+     */
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
     @Override
     public boolean getValue() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return value;
     }
 
-    @Override
-    public void addPropertyChangeListener(PropertyChangeListener pl) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void removePropertyChangeListener(PropertyChangeListener pl) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
