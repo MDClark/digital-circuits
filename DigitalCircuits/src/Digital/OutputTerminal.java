a1176235ca3fee1f5f0ec8da0787b19a7e089a5a
MDClark <m14.clark@connect.qut.edu.au>
2014-08-05 19:57:49 +1000

    Init commit
    Setup partially completed (Part A)

---
+++ DigitalCircuits/src/Digital/OutputTerminal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Digital;
+
+import java.beans.PropertyChangeListener;
+
+/**
+ *
+ * @author Clarky
+ */
+public class OutputTerminal implements Terminal {
+
+    @Override
+    public boolean getValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener pl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener pl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
